639cdf5b8175b1bd8032b97b182ffa01c54ca2e0
Brian Kurilko <briankurilko@gmail.com>
2023-09-01 09:41:06 -0400

    first commit

---
+++ src/main/java/com/example/matt/MattApplication.java
@@ -0,0 +1,13 @@
+package com.example.matt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/matt/controller/MattController.java
@@ -0,0 +1,18 @@
+package com.example.matt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class MattController {
+
+    @GetMapping("/hello-world")
+    @ResponseBody
+    public String helloWorld() {
+        return "Matt";
+    }
+}
